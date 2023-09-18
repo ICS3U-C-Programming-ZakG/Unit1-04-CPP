7bdb11f6ea33de198c12f1462df1ebf1222e7f9d
Zak Goneau <zak.goneau@stu.ocsb.ca>
2023-09-18 22:57:57 +0000

    Do basic math and calculate the area and perimeter of a rectangle in C++.

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// !/usr/bin/env python3
+
+// Created by: Zak Goneau
+// Created on: Sep. 18, 2023
+// This program calculates the perimeter and area of a rectangle with dimensions of 5.7cm x 9cm.
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions:" << std::endl;
+    std::cout << "5.7cm x 9cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "The area is: " << (5.7 * 9) << "cm^2" << std::endl;
+    std::cout << "The perimeter is: " << (2 * (5.7 + 9)) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 Zak Goneau All rights reserved
+//
+// Created by: Zak Goneau
+// Created on: Sep. 18, 2023
+// This program does basic math.
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "24+13= " << (24 + 13) << std::endl;
+    std::cout << "19-12= " << (19 - 12) << std::endl;
+    std::cout << "27.3/4.6= " << (27.3 / 4.6) << std::endl;
+    std::cout << "pow(5,3)= " << pow(5, 3) << std::endl;
+    std::cout << "sqrt(256)= " << sqrt(256) << std::endl;
+}
